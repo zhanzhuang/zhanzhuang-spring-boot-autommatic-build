3b3bea19015d01d9d6a83c3baf70166bbdc8dae8
Jensen <jensen.zhan@schindler.com>
2020-05-25 17:46:48 +0800

    log

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -13,6 +13,6 @@ public class TestController {
     @GetMapping("test")
     public String test() {
 //        return "向github提交代码触发jenkins自动构建";
-        return "向github提交代码触发jenkins自动构建测试";
+        return "向github提交代码触发jenkins自动构建测试   ";
     }
 }
