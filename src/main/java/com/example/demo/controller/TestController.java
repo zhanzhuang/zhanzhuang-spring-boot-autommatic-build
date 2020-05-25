60176aaed817c45af90c3b3d1445bca75a20aaf7
Jensen <jensen.zhan@schindler.com>
2020-05-25 16:03:12 +0800

    log

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Jensen Zhan
+ */
+@RestController
+public class TestController {
+
+    @GetMapping("test")
+    public String test() {
+        return "向github提交代码触发jenkins自动构建";
+    }
+}
